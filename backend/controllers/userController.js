350b7f93ae5779c16d169baebf6bfde6f60c5e16
Atul Kr Karn <atul48kumar90@gmail.com>
2022-10-16 21:09:50 +0530

    userapp trial finished

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,10 @@
 const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel');
 
 const getUsers = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Get users list'})
+
+    const users = await User.find();
+    res.status(200).json(users)
 })
 
 const addUser = asyncHandler(async (req, res) => {
@@ -10,15 +13,37 @@ const addUser = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('please add text field')
     }
-    res.status(200).json({message: 'User added'})
+    const user = await User.create({
+        text: req.body.text
+    })
+    res.status(200).json(user)
 })
 
 const updateUser = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `update user ${req.params.id}`})
+
+    const user = await User.findById(req.params.id)
+
+    if(!user)
+    {
+        res.status(400)
+        throw new Error('User not found')
+    }
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+        new: true
+    })
+    res.status(200).json(updatedUser)
 })
 
 const deleteUser = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'User deleted'})
+    const user = await User.findById(req.params.id)
+
+    if(!user)
+    {
+        res.status(400)
+        throw new Error('User not found')
+    }
+    await user.remove()
+    res.status(200).json({id:req.params.id})
 })
 
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const dotenv = require('dotenv').config();
 const { errHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
 
 const PORT = 5000;
 
 const app = express()
+connectDB()
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
